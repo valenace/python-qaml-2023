96c2ef2ac38d4f03be324fd979620b15b91856c9
valenaceg <valentina@qamindslab.com>
2023-02-17 19:52:50 -0400

    Actividad: clase 14/02

---
+++ python_03.py
@@ -0,0 +1,15 @@
+nombre_user = input('¿Cuál es tu nombre? ')
+
+edad_user = input('¿Qué edad tienes? ')
+
+ciudad_user = input('¿Cuál es tu ciudad? ')
+
+edo_civil = input('¿Estás casado? ')
+
+num_1_100 = input('Ingrese un número entre el 1 al 100 ')
+
+num_100_200 = input('Ingrese un número entre el 100 al 200 ')
+
+division= (int(num_1_100) / int(num_100_200))
+
+print(f'Mi nombre es {nombre_user}, mi edad es {edad_user}, soy de la ciudad de {ciudad_user}, y mi resultado es {division}')
